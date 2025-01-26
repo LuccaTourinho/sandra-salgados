2b894f1baa5153de1c0ee8a3f11f6ea972dfad11
Lucca Tourinho <luccatourinho@gmail.com>
2025-01-26 13:25:51 -0300

    Pequenas alteracoes

--- src/components/EventSwipper.tsx
+++ src/components/EventSwipper.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";   
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 interface eventProps {
     id: number;
@@ -46,10 +47,10 @@ const EventSwipper = () => {
     return (
         <div className="flex flex-col items-center py-8 gap-8">
             <div className="flex flex-row items-center justify-around gap-8">
-                <Button size={'sm'} className="text-sm" onClick={handlePrev}>
-                    Left
-                </Button>
-                <div className="relative w-[300px] h-[300px] md:w-[400px] md:h-[500px] xl:w-[600px] xl:h-[800px] overflow-hidden border-1 border-text-950 rounded-lg">
+                <div className="bg-primary-500 rounded-lg p-3 hover:bg-primary-600 hover:cursor-pointer transition-all duration-300" onClick={handlePrev}>
+                    <ArrowLeft size={36} strokeWidth={3}/>
+                </div>
+                <div className="relative w-[300px] h-[300px] md:w-[500px] md:h-[500px] xl:w-[800px] xl:h-[1000px] 2xl:w-[1000px] 2xl:h-[1200px] overflow-hidden border-1 border-text-950 rounded-lg">
                     <div className="absolute inset-0 flex items-center justify-center">
                         {
                             events.map((event, index) => {
@@ -81,9 +82,9 @@ const EventSwipper = () => {
                         }
                     </div>
                 </div>
-                <Button size={'sm'} className="text-sm" onClick={handleNext}>
-                    Right
-                </Button>
+                <div className="bg-primary-500 rounded-lg p-3 hover:bg-primary-600 hover:cursor-pointer transition-all duration-300" onClick={handleNext}>
+                    <ArrowRight size={36} strokeWidth={3}/>
+                </div>
             </div>
             <p className="text-md font-extralight text-text-950">
                 {events[activeIndex].description}
